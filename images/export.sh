7f1201e9ef1452332ca1323a4cc2e2000e3d5b6a
Tristan Glatard <tristan.glatard@mcgill.ca>
2016-04-08 15:50:09 -0400

    Added figures, updates in paper.tex

---
+++ images/export.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for name in tight service
+do
+  echo "Exporting ${name}.svg"
+  inkscape -D -z --file=images/${name}.svg --export-pdf=images/${name}.pdf --export-latex
+  sed s,${name}\.pdf,images/${name}\.pdf,g images/${name}.pdf_tex > images/${name}.pdf_tex.new
+  \mv images/${name}.pdf_tex.new images/${name}.pdf_tex
+done
