a29486ab1b7b8fb1c99b77339c307ad78d01a795
Tristan Glatard <tristan.glatard@mcgill.ca>
2016-04-12 09:06:21 -0400

    Renamed images folder into figures

--- figures/export.sh
+++ figures/export.sh
@@ -3,7 +3,7 @@
 for name in tight service sub-task agent nested-1 nested-2 import-1 import-2
 do
   echo "Exporting ${name}.svg"
-  inkscape -D -z --file=images/${name}.svg --export-pdf=images/${name}.pdf --export-latex
-  sed s,${name}\.pdf,images/${name}\.pdf,g images/${name}.pdf_tex > images/${name}.pdf_tex.new
-  \mv images/${name}.pdf_tex.new images/${name}.pdf_tex
+  inkscape -D -z --file=figures/${name}.svg --export-pdf=figures/${name}.pdf --export-latex
+  sed s,${name}\.pdf,figures/${name}\.pdf,g figures/${name}.pdf_tex > figures/${name}.pdf_tex.new
+  \mv figures/${name}.pdf_tex.new figures/${name}.pdf_tex
 done
